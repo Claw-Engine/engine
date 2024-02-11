c19bde9cf22dc59c28533d7ebc17de570248d95b
Lucas Souto <contato.lucassouto@outlook.com>
2024-02-11 18:31:41 -0300

    Removendo testes de UI.

--- Tests/Main.cs
+++ Tests/Main.cs
@@ -26,43 +26,11 @@ namespace Tests
         private void LoadContent()
         {
             Asset.Load<Sprite[]>("MainAtlas");
-            NineSlice.AddTexture("base", TextureAtlas.Sprites["MainAtlas/slice"]);
 
             Font = Asset.Load<SpriteFont>("Fonts/font");
-            UI = new UI();
-            UI.Body = new Container() { Style = new Style() { Size = new Vector2(128), Gap = new Vector2(8), TopLeftPadding = new Vector2(8), BottomRightPadding = new Vector2(8), NineSlice = "base" } };
-            UI.Body.Scrollable = true;
-            UI.Cursor = new UICursor();
-
-            Container other = new Container() { Scrollable = true, Style = new Style() { NineSlice = "base", Color = Color.Yellow, Display = Display.Block, TopLeftPadding = new Vector2(4), BottomRightPadding = new Vector2(4), Gap = new Vector2(4), Size = new Vector2(128 - 16, 64) } };
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Magenta, Display = Display.Inline } });
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Magenta, Display = Display.Inline } });
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Blue, Display = Display.Inline } });
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Magenta, Display = Display.Inline } });
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Magenta, Display = Display.Inline } });
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Blue, Display = Display.Inline } });
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Magenta, Display = Display.Inline } });
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Magenta, Display = Display.Inline } });
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Blue, Display = Display.Inline } });
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Magenta, Display = Display.Inline } });
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Magenta, Display = Display.Inline } });
-            other.Elements.Add(new Container() { Style = new Style() { NineSlice = "base", Size = new Vector2(24), Color = Color.Blue, Display = Display.Inline } });
-            UI.Body.Elements.Add(new Slider() { Style = new Style() { Color = Color.Black }, Sync = (value) => other.ScrollOffset = new Vector2(other.ScrollMaxOffset.X * value, 0) });
-            UI.Body.Elements.Add(other);
-            UI.Recalculate();
         }
         
-        protected override void Step()
-        {
-            UI.Cursor.Position = Input.MousePosition;
-
-            updateables.ForEach((u) => u.Step());
-
-            if (Input.KeyPressed(Keys.Right)) ((Slider)UI.Body.Elements[0]).Value += .1f;
-            else if (Input.KeyPressed(Keys.Left)) ((Slider)UI.Body.Elements[0]).Value -= .1f;
-
-            if (Input.MouseButtonDown(MouseButtons.Left) && UI.Cursor.Hover is Slider slider) slider.SetValue(UI.Cursor.HoverDifference);
-        }
+        protected override void Step() => updateables.ForEach((u) => u.Step());
 
         protected override void Render() => drawables.ForEach((d) => d.Render());
     }
