289df6f2554b6c377a82992434e1fb8b93cb5ca0
Lucas Souto <contato.lucassouto@outlook.com>
2024-02-14 09:43:12 -0300

    Correções e começando leitura de comentários.

--- Clawssets/Builder/Readers/BitReader.cs
+++ Clawssets/Builder/Readers/BitReader.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.IO;
 using System.Collections;
+using System.Text;
 
 namespace Clawssets.Builder.Readers
 {
+    /// <summary>
+    /// Implementação Stream de leitura de bits isolados.
+    /// </summary>
     public class BitReader
     {
         public bool BigEndian;
@@ -17,11 +21,43 @@ namespace Clawssets.Builder.Readers
             currentByte = (byte)stream.ReadByte();
         }
 
-        public void JumpBytes(uint count)
+        /// <summary>
+        /// Pula um determinado número de bits.
+        /// </summary>
+        public void JumpBits(uint count)
         {
-            for (int i = 0; i < 8 * count; i++) ReadBit();
+            for (int i = 0; i < count; i++) ReadBit();
+        }
+        /// <summary>
+        /// Pula um determinado número de bytes.
+        /// </summary>
+        public void JumpBytes(uint count) => JumpBits(8 * count);
+        
+        /// <summary>
+        /// Volta 1 bit.
+        /// </summary>
+        public void BackBit()
+        {
+            if (bit == 0)
+            {
+                bit = 7;
+                BaseStream.Position--;
+                currentByte = (byte)BaseStream.ReadByte();
+            }
+            else bit--;
+        }
+        /// <summary>
+        /// Volta 8 bits.
+        /// </summary>
+        public void BackBytes(long count)
+        {
+            BaseStream.Position -= count;
+            currentByte = (byte)BaseStream.ReadByte();
         }
 
+        /// <summary>
+        /// Lê 1 bit.
+        /// </summary>
         public bool ReadBit()
         {
             if (bit == 8)
@@ -30,29 +66,95 @@ namespace Clawssets.Builder.Readers
                 currentByte = (byte)BaseStream.ReadByte();
             }
 
-            bool value;
-
-            if (BigEndian) value = (currentByte & (1 << bit)) > 0;
-            else value = (currentByte & (1 << (7 - bit))) > 0;
+            bool value = (currentByte & (1 << (7 - bit))) > 0;
             
             bit++;
 
             return value;
         }
+        /// <summary>
+        /// Lê 8 bits, sem sinal.
+        /// </summary>
         public byte ReadByte() => (byte)ReadBits(8);
-        public sbyte ReadSByte() => (sbyte)ReadBits(8);
-        public short ReadInt16() => (short)ReadBits(16);
-        public ushort ReadUInt16() => (ushort)ReadBits(16);
-        public Int24 ReadInt24() => (int)ReadBits(24);
-        public int ReadInt32() => (int)ReadBits(32);
-        public uint ReadUInt32() => (uint)ReadBits(32);
-        public long ReadInt64() => (long)ReadBits(64);
-        public ulong ReadUInt64() => (ulong)ReadBits(64);
-        private long ReadBits(int count)
+        /// <summary>
+        /// Lê 1 char.
+        /// </summary>
+        public char ReadChar() => (char)ReadByte();
+        /// <summary>
+        /// Lê uma string.
+        /// </summary>
+        public string ReadString(int length)
         {
-            long value = 0;
+            StringBuilder builder = new StringBuilder();
 
-            for (byte i = 0; i < count; i++)
+            for (int i = 0; i < length; i++) builder.Append(ReadChar());
+
+            return builder.ToString();
+        }
+        /// <summary>
+        /// Lê 8 bits, com sinal.
+        /// </summary>
+        public sbyte ReadSByte() => (sbyte)ReadBits(8);
+        /// <summary>
+        /// Lê 16 bits, com sinal.
+        /// </summary>
+        public short ReadInt16() => (short)ReadBits(16);
+        /// <summary>
+        /// Lê 16 bits, sem sinal.
+        /// </summary>
+        public ushort ReadUInt16() => (ushort)ReadBits(16);
+        /// <summary>
+        /// Lê 24 bits, com sinal.
+        /// </summary>
+        public Int24 ReadInt24() => (int)ReadBits(24);
+        /// <summary>
+        /// Lê 32 bits, com sinal.
+        /// </summary>
+        public int ReadInt32() => (int)ReadBits(32);
+        /// <summary>
+        /// Lê 32 bits, sem sinal.
+        /// </summary>
+        public uint ReadUInt32() => (uint)ReadBits(32);
+        /// <summary>
+        /// Lê 64 bits, com sinal.
+        /// </summary>
+        public long ReadInt64() => (long)ReadBits(64);
+        /// <summary>
+        /// Lê 64 bits, sem sinal.
+        /// </summary>
+        public ulong ReadUInt64() => (ulong)ReadBits(64);
+        /// <summary>
+        /// Lê até 64 bits, como um <see cref="long"/>.
+        /// </summary>
+        public ulong ReadBits(byte count, bool log = false)
+        {
+            ulong value = 0;
+
+            if (count > 8 && count % 8 == 0)
+            {
+                byte[] bytes = new byte[count / 8];
+                sbyte byteIndex = -1;
+
+                for (byte i = 0; i < count; i++)
+                {
+                    if (i % 8 == 0) byteIndex++;
+
+                    bytes[byteIndex] <<= 1;
+
+                    if (ReadBit()) bytes[byteIndex] |= 1;
+                }
+
+                if (BitConverter.IsLittleEndian == BigEndian) Array.Reverse(bytes); // Se a ordem de bytes do arquivo for diferente da do sistema
+                
+                switch (count)
+                {
+                    case 16: value = BitConverter.ToUInt16(bytes, 0); break;
+                    case 24: value = BitConverter.ToUInt32(bytes, 0); break;
+                    case 32: value = BitConverter.ToUInt32(bytes, 0); break;
+                    case 64: value = BitConverter.ToUInt64(bytes, 0); break;
+                }
+            }
+            else for (byte i = 0; i < count; i++)
             {
                 value <<= 1;
 
--- Clawssets/Builder/Readers/OggReader.cs
+++ Clawssets/Builder/Readers/OggReader.cs
@@ -25,29 +25,22 @@ namespace Clawssets.Builder.Readers
         /// </summary>
         public static Audio.Description Load(BitReader reader)
         {
-            reader.ReadSignature();
-
             Audio.Description audio = new Audio.Description();
 
             IdHeader idHeader = reader.ReadIdHeader(audio);
-
+            
+            reader.ReadCommentHeader();
+            while (true) { }
             return null;
         }
 
-        private static void ReadSignature(this BitReader reader)
-        {
-            reader.JumpBytes(4);// OggS
-
-            byte version = reader.ReadByte(), flags = reader.ReadByte();
-            ulong granulePosition = reader.ReadUInt64();
-            uint serialNumber = reader.ReadUInt32(), sequenceNumber = reader.ReadUInt32(), checkSum = reader.ReadUInt32();
-            byte totalSegments = reader.ReadByte();
-
-            reader.JumpBytes(1);
-        }
         private static void JumpCommon(this BitReader reader) => reader.JumpBytes(7);// PacketType-Vorbis
+        private static void JumpSignature(this BitReader reader) => reader.JumpBytes(4 + 1 + 1 + 8 + 4 + 4 + 4 + 1);
         private static IdHeader ReadIdHeader(this BitReader reader, Audio.Description audio)
         {
+            reader.JumpSignature();
+            reader.JumpBytes(1);
+
             IdHeader idHeader = new IdHeader();
 
             reader.JumpCommon();
@@ -61,7 +54,39 @@ namespace Clawssets.Builder.Readers
             idHeader.BlockSizes = reader.ReadByte();
             idHeader.FramingFlag = reader.ReadBit();
 
+            if ((char)reader.ReadByte() != 'O')
+            {
+                reader.BackBytes(2);
+                reader.BackBit();
+                
+                idHeader.FramingFlag = reader.ReadByte() == 1;
+            }
+            else reader.BackBytes(2);
+            
+            if (idHeader.BitrateNominal == 0 && idHeader.BitrateMaximum > 0 && idHeader.BitrateMinimum > 0) idHeader.BitrateNominal = (idHeader.BitrateMaximum + idHeader.BitrateMinimum) / 2;
+            
             return idHeader;
+        }
+        private static void ReadCommentHeader(this BitReader reader)
+        {
+            reader.JumpSignature();
+            reader.JumpCommon();
+
+            uint vendorLength = reader.ReadUInt32();
+
+            reader.JumpBytes(vendorLength);
+
+            uint userCommentListLength = reader.ReadUInt32();
+            uint len;
+            
+            for (uint i = 0; i < userCommentListLength; i++)
+            {
+                len = reader.ReadUInt32();
+                
+                reader.JumpBytes(len);
+            }
+
+            reader.ReadBit();
         }
     }
 }
