7e86c932cb429bc3a5b448180929cfb5be60e7f0
Lucas Souto <contato.lucassouto@outlook.com>
2024-03-20 12:33:24 -0300

    Separação revertida + fechamento do source.

---
+++ Clawssets/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Clawssets
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			using (Main game = new Main()) game.Run();
+		}
+	}
+}
---
+++ Clawssets/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Clawssets")]
+[assembly: AssemblyDescription("Compilador de assets da Claw Engine.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Claw")]
+[assembly: AssemblyProduct("Clawssets")]
+[assembly: AssemblyCopyright("Copyright © Clawssets 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f044d96-7508-4255-8289-5bb36397bad8")]
+
+[assembly: AssemblyVersion("2.0.0")] // Main.Release.SubRelease
