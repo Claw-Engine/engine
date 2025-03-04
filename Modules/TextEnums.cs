f7a20b9ae2b2f12ec775a1144ad2558dbae185d8
Lucas Souto <contato.lucassouto@outlook.com>
2025-03-04 09:32:07 -0300

    Movendo mais coisas 2.

---
+++ Modules/TextEnums.cs
@@ -0,0 +1,25 @@
+namespace Claw.Modules;
+
+/// <summary>
+/// Define os tipo de origem possíveis para o <see cref="TextRenderer"/>.
+/// </summary>
+public enum TextOrigin
+{
+	TopLeft, Top, TopRight,
+	Left, Center, Right,
+	BottomLeft, Bottom, BottomRight
+}
+/// <summary>
+/// Define os tipos de efeito do <see cref="TextRenderer"/>.
+/// </summary>
+public enum TextEffect
+{
+	None,
+	Wave, Scream,
+	Pulsate, Rotation,
+	MovingHorizontal, MovingVertical
+}
+/// <summary>
+/// Define os tipos de quebra de linha do <see cref="TextRenderer"/>.
+/// </summary>
+public enum TextWrap { NoWrap, Anywhere, Word }
