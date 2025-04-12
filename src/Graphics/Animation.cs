633411d4d870f9a77cad54b29cd9e2f81b09d3dc
Lucas Souto <contato.lucassouto@outlook.com>
2025-04-12 07:23:06 -0300

    Breaking: Adição de margem para Animation.Generate*.

--- src/Graphics/Animation.cs
+++ src/Graphics/Animation.cs
@@ -21,7 +21,7 @@ public sealed class Animation
 	/// <summary>
 	/// Gera animação com um spritesheet horizontal.
 	/// </summary>
-	public static Animation[] GenerateHorizontal(Sprite spriteSheet, int amount, int[] frames, int[] animationFPS, string[] names, Vector2[] origins, Vector2 cellSize, Vector2 offset)
+	public static Animation[] GenerateHorizontal(Sprite spriteSheet, int amount, int[] frames, int[] animationFPS, string[] names, Vector2[] origins, Vector2 cellSize, Vector2 offset, Vector2 margin)
 	{
 		Animation[] anim = new Animation[amount];
 
@@ -29,7 +29,7 @@ public sealed class Animation
 		{
 			List<Frame> framesL = new List<Frame>();
 
-			for (int j = 0; j < frames[i]; j++) framesL.Add(new Frame(spriteSheet, new Rectangle(offset + new Vector2(j * cellSize.X, i * cellSize.Y), cellSize)));
+			for (int j = 0; j < frames[i]; j++) framesL.Add(new Frame(spriteSheet, new Rectangle(offset + new Vector2(j * cellSize.X, i * cellSize.Y) + margin, cellSize - margin * 2)));
 
 			anim[i] = new Animation(animationFPS[i], names[i], origins[i], framesL.ToArray());
 		}
@@ -39,7 +39,7 @@ public sealed class Animation
 	/// <summary>
 	/// Gera animação com um spritesheet horizontal.
 	/// </summary>
-	public static Animation[] GenerateHorizontal(Sprite spriteSheet, int amount, int frames, int animationFPS, Vector2 origin, Vector2 cellSize, Vector2 offset, params string[] names)
+	public static Animation[] GenerateHorizontal(Sprite spriteSheet, int amount, int frames, int animationFPS, Vector2 origin, Vector2 cellSize, Vector2 offset, Vector2 margin, params string[] names)
 	{
 		Animation[] anim = new Animation[amount];
 
@@ -47,7 +47,7 @@ public sealed class Animation
 		{
 			List<Frame> framesL = new List<Frame>();
 
-			for (int j = 0; j < frames; j++) framesL.Add(new Frame(spriteSheet, new Rectangle(offset + new Vector2(j * cellSize.X, i * cellSize.Y), cellSize)));
+			for (int j = 0; j < frames; j++) framesL.Add(new Frame(spriteSheet, new Rectangle(offset + new Vector2(j * cellSize.X, i * cellSize.Y) + margin, cellSize - margin * 2)));
 
 			anim[i] = new Animation(animationFPS, names[i], origin, framesL.ToArray());
 		}
@@ -58,7 +58,7 @@ public sealed class Animation
 	/// <summary>
 	/// Gera animação com um spritesheet vertical.
 	/// </summary>
-	public static Animation[] GenerateVertical(Sprite spriteSheet, int amount, int[] frames, int[] animationFPS, string[] names, Vector2[] origins, Vector2 cellSize, Vector2 offset)
+	public static Animation[] GenerateVertical(Sprite spriteSheet, int amount, int[] frames, int[] animationFPS, string[] names, Vector2[] origins, Vector2 cellSize, Vector2 offset, Vector2 margin)
 	{
 		Animation[] anim = new Animation[amount];
 
@@ -66,7 +66,7 @@ public sealed class Animation
 		{
 			List<Frame> framesL = new List<Frame>();
 
-			for (int j = 0; j < frames[i]; j++) framesL.Add(new Frame(spriteSheet, new Rectangle(offset + new Vector2(i * cellSize.X, j * cellSize.Y), cellSize)));
+			for (int j = 0; j < frames[i]; j++) framesL.Add(new Frame(spriteSheet, new Rectangle(offset + new Vector2(i * cellSize.X, j * cellSize.Y) + margin, cellSize - margin * 2)));
 
 			anim[i] = new Animation(animationFPS[i], names[i], origins[i], framesL.ToArray());
 		}
@@ -76,7 +76,7 @@ public sealed class Animation
 	/// <summary>
 	/// Gera animação com um spritesheet vertical.
 	/// </summary>
-	public static Animation[] GenerateVertical(Sprite spriteSheet, int amount, int frames, int animationFPS, Vector2 origin, Vector2 cellSize, Vector2 offset, params string[] names)
+	public static Animation[] GenerateVertical(Sprite spriteSheet, int amount, int frames, int animationFPS, Vector2 origin, Vector2 cellSize, Vector2 offset, Vector2 margin, params string[] names)
 	{
 		Animation[] anim = new Animation[amount];
 
@@ -84,7 +84,7 @@ public sealed class Animation
 		{
 			List<Frame> framesL = new List<Frame>();
 
-			for (int j = 0; j < frames; j++) framesL.Add(new Frame(spriteSheet, new Rectangle(offset + new Vector2(i * cellSize.X, j * cellSize.Y), cellSize)));
+			for (int j = 0; j < frames; j++) framesL.Add(new Frame(spriteSheet, new Rectangle(offset + new Vector2(i * cellSize.X, j * cellSize.Y) + margin, cellSize - margin * 2)));
 
 			anim[i] = new Animation(animationFPS, names[i], origin, framesL.ToArray());
 		}
